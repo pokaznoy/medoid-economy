bdda572a2c2ee48d14460be2d261ac32b205017b
pokaznoy <109614768+pokaznoy@users.noreply.github.com>
2026-04-29 22:57:03 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+const medoids = [
+  {
+    name: "DroneTech",
+    owner: "Роман",
+    category: "IT",
+    description: "Розробка дронів та електроніки",
+    link: "https://t.me/example"
+  },
+  {
+    name: "WoodCraft",
+    owner: "Іван",
+    category: "Будівництво",
+    description: "Дерев'яні конструкції та меблі",
+    link: "https://t.me/example2"
+  }
+];
