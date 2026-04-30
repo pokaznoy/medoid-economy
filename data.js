5d4bb3b5868fa03cd10ec8aee14848b8a670ea08
pokaznoy <109614768+pokaznoy@users.noreply.github.com>
2026-04-30 20:50:24 +0300

    Auto update data.js from Google Sheets

--- data.js
+++ data.js
@@ -1,12 +1,5 @@
 const medoids = [
   {
-    "business_name": "kalimba time",
-    "owner": "Роман Петричук",
-    "category": "Культура",
-    "description": "Виготовлення музичних інструментів, на разі калімби",
-    "link": "https://www.instagram.com/kalimba_time?igsh=MTB5MDB6ZWFmcXBlaQ=="
-  },
-  {
     "business_name": "Ryshelie",
     "owner": "Mariia",
     "category": "Текстиль",
